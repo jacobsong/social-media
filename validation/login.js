1e46592d9e32df8d025c0db6f17530e77bf633aa
Jacob Song <jsong@aqsongjm01.rsn.aac.usarec.army.mil>
2018-05-14 17:27:46 -0400

    validation

---
+++ validation/login.js
@@ -0,0 +1,23 @@
+const Validator = require("validator");
+const _ = require("lodash");
+
+module.exports = function validateLoginInput(data) {
+  let errors = {};
+
+  if (!Validator.isEmail(data.email)) {
+    errors.email = "Email is invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email field is required";
+  }
+
+  if (Validator.isEmpty(data.password)) {
+    errors.password = "Password field is required";
+  }
+
+  return {
+    errors,
+    isValid: _.isEmpty(errors)
+  };
+};
