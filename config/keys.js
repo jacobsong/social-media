e4bda13a707847200bc8aaf84e474d53cfffb46c
Jacob Song <jacobsong@Jacobs-MacBook-Pro.local>
2018-05-20 00:16:24 -0400

    added dashboard

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  //mongoURI: "mongodb://jake:jake@ds217350.mlab.com:17350/jms-devconnector",
-  mongoURI: "mongodb://jake:jake@localhost:27017/devconnector",
+  mongoURI: "mongodb://jake:jake@ds217350.mlab.com:17350/jms-devconnector",
+  //mongoURI: "mongodb://jake:jake@localhost:27017/devconnector",
   secretOrKey: "secret"
 };
