c7de87762194a32bafcb1fa33a2f465377da4d45
Jacob Song <jsong@aqsongjm01.rsn.aac.usarec.army.mil>
2018-05-21 17:22:14 -0400

    added createprofile

--- server.js
+++ server.js
@@ -33,4 +33,6 @@ app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app
+  .listen(port, () => console.log(`Server running on port ${port}`))
+  .on("error", console.log);
