37b97e17b54a037af9c875ffe8849bc262f10b5f
Jacob Song <jsong@aqsongjm01.rsn.aac.usarec.army.mil>
2018-05-25 10:28:58 -0400

    removed devtools from production

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,20 @@ const initialState = {};
 
 const middleware = [thunk];
 
+let devTools;
+
+if (process.env.NODE_ENV === "production") {
+  devTools = null;
+} else {
+  devTools =
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__();
+}
+
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  compose(applyMiddleware(...middleware), devTools)
 );
 
 export default store;
